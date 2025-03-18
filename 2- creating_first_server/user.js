acb7b7302b3b7f5c75c1e8d1d74deba350d47d6a
Aziz Ur Rehman <aziz.ur.rehman.gill@gmail.com>
2025-03-19 01:16:30 +0500

    make a file and save user's input into the file

--- 2- creating_first_server/user.js
+++ 2- creating_first_server/user.js
@@ -1,4 +1,5 @@
 import http from 'http';
+import fs from 'fs';
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
@@ -7,8 +8,8 @@ const server = http.createServer((req, res) => {
         res.write("<html>");
         res.write("<head><title>My First Page</title></head>");
         res.write("<body><h1>Enter your details here: </h1>");
-        res.write('<from action="/submit-detail" method="POST">');
-        res.write('<input type="text name="username" placeholder="Enter your name">');
+        res.write('<form action="/submit-detail" method="POST">');
+        res.write('<input type="text" name="username" placeholder="Enter your name">');
         res.write('<label for="male">Male:</label>');
         res.write('<input type="radio" id="male" name="gender" value="male">');
         res.write('<label for="male">Female:</label>');
@@ -18,7 +19,12 @@ const server = http.createServer((req, res) => {
         res.write('</body>');
         res.write("</html>");
         return res.end();  // must write this after end of every req, res
-    } else if(req.url==='/submit-details' && req.method==='POST'){
+    } else if(req.url==='/submit-detail' && req.method==='POST'){
+        // now created a file and saved the user's input into the user.txt file 
+        fs.writeFileSync('user.txt', 'Aziz Ur Rehman');
+        res.statusCode = 302;
+        // we can also set header as: 
+        // res.setHeader('Location', '/');
         res.setHeader("Content-Type", "text/html");
         res.write("<html>");
         res.write("<head><title>My First Page</title></head>");
