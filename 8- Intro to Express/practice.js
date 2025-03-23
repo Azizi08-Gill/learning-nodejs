f81401760de4a707deac2b9051ad88755597ec03
Aziz Ur Rehman <aziz.ur.rehman.gill@gmail.com>
2025-03-23 22:15:34 +0500

    a simple task to understand routing in Express

---
+++ 8- Intro to Express/practice.js
@@ -0,0 +1,51 @@
+import express from 'express';
+
+const app = express();
+
+app.use((req, res, next)=>{
+    console.log("In first middleware...", req.url, req.method);
+    next();
+})
+
+app.use((req, res, next)=>{
+    console.log("In second middleware...", req.url, req.method);
+    next();
+})
+
+// app.use((req, res, next)=>
+//     console.log("Inside third middleware...");
+//     res.send("<h1>Response from third middleware</h1>");
+// })
+
+app.get('/',(req, res, next)=>{
+   res.send("<h1>Welcome to Express JS</h1>");
+})
+
+app.get('/contact-us',(req, res, next)=>{
+    res.send(`
+        <h1>Please Give Your Details Here</h1>
+        <form method="POST" action="/submit-details">
+        <input type="text" name="name" placeholder="Enter your name">
+        <input type="email" name="email" placeholder="Enter your email">
+        <input type="submit" value="Submit">
+        `);
+ })
+
+ app.get('/submit-details',(req, res, next)=>{
+    res.send(`
+        <h1>Please Give Your Details Here</h1>
+        <form method="POST" action="/submit-details">
+        <input type="text" name="name" placeholder="Enter your name">
+        <input type="email" name="email" placeholder="Enter your email">
+        <input type="submit" value="Submit">
+        `);
+ });
+
+    app.post("/submit-details", (req, res, next) => {
+      console.log(req.body);
+      res.send("<h1>Thank you for submitting your details</h1>");
+    });
+const PORT = 9000;
+app.listen(PORT, ()=>{
+    console.log(`Server running at http://localhost:${PORT}`);
+})
