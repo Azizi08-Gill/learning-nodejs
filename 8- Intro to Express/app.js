2fc2972ed77ac4bd90dddf69604a0a57fbd6dc25
Aziz Ur Rehman <aziz.ur.rehman.gill@gmail.com>
2025-03-23 21:12:05 +0500

    Express handling server

--- 8- Intro to Express/app.js
+++ 8- Intro to Express/app.js
@@ -1,9 +1,28 @@
-const http = require('http');
-const  handler  = require('./handler');
+// Core Modules
+//const http = require('http');
+// No need for http module, express will handle everything for us
 
-const server = http.createServer(handler);
+// External Module
+const express = require('express');
+// Local Module
+//const requestHandler = require('./user');
 
-const PORT = 3009;
-server.listen(PORT, ()=>{
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("In first middleware...", req.url, req.method);
+    next();
+})
+
+app.use((req, res, next) => {
+    console.log("In second middleware...", req.url, req.method);
+    res.send("<p>Welcome to Express JS</p>")
+})
+
+
+const PORT = 3007;
+
+app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
