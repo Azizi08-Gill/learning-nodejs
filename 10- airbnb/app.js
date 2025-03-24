108eb4d43943429a4106fccc73da6f6a0f8a82bd
Aziz Ur Rehman <aziz.ur.rehman.gill@gmail.com>
2025-03-25 02:08:11 +0500

    add a contact-us page

--- 10- airbnb/app.js
+++ 10- airbnb/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userRouter = require('./routes/userRouter');
 const hostRouter = require('./routes/hostRouter');
 const rootDir = require('./utils/pathUtils');
+const contactRouter = require('./routes/contactRouter');
 
 //core module
 const path = require('path');
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(userRouter);
 app.use("/host",hostRouter);
+app.use(contactRouter);
 
 app.use((req, res, next)=>{
     res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
