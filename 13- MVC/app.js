38e8842105c2dfd0f3b2cfc32c8ae13f4609d7e8
Aziz Ur Rehman <aziz.ur.rehman.gill@gmail.com>
2025-03-30 06:41:04 +0500

    error controllers

--- 13- MVC/app.js
+++ 13- MVC/app.js
@@ -3,6 +3,7 @@ const userRouter = require('./routes/userRouter');
 const hostRouter = require('./routes/hostRouter');
 const rootDir = require('./utils/pathUtils'); // no need for rootDir anymore
 const contactRouter = require('./routes/contactRouter');
+const errorControllers = require('./controllers/errors');
 
 //core module
 const path = require('path');
@@ -18,9 +19,7 @@ app.use(userRouter);
 app.use("/host",hostRouter);
 app.use(contactRouter);
 
-app.use((req, res, next)=>{
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
-});
+app.use(errorControllers.errorPage);
 
 const PORT = 9000;
 app.listen(PORT, () => {
--- 13- MVC/controllers/homes.js
+++ 13- MVC/controllers/homes.js
@@ -15,3 +15,7 @@ exports.getHomes = (req, res) => {
     console.log(registeredHomes);
     res.render('home', {registeredHomes: registeredHomes, pageTitle: 'airbnb'});
 };
+
+exports.errorPage = (req, res, next)=>{
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+};
